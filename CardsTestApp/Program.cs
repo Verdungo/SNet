cb7a51facd974c2e39f8eea037c2b44f115031e4
Verdungo <myxomop.k66@gmail.com>
2015-11-16 19:02:21 +0500

    bdsb

---
+++ CardsTestApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using PlayingCards.Card;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardsTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Card> deck = new List<Card>();
+            for (int i = 0; i < 52; i++)
+            {
+                deck.Add(new Card(i));
+            }
+
+            for (int j = 0; j < 52; j++)
+            {
+                Console.WriteLine(String.Format("{0} \t\t {1}", deck[j].FullName , deck[j].ShortName));
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
--- PlayingCards/Card/Card.cs
+++ PlayingCards/Card/Card.cs
@@ -17,6 +17,54 @@ namespace PlayingCards.Card
 
         public string FullName => String.Format("{0} of {1}", Rank.ToString(), Suit.ToString());
 
+        public string ShortName
+        {
+            get
+            {
+                string res = "";
+                switch (Rank)
+                {
+                    case Rank.Ace:
+                        res += "A";
+                        break;
+                    case Rank.Ten:
+                        res += "T";
+                        break;
+                    case Rank.Jack:
+                        res += "J";
+                        break;
+                    case Rank.Queen:
+                        res += "Q";
+                        break;
+                    case Rank.King:
+                        res += "K";
+                        break;
+                    default:
+                        res += ((int)Rank).ToString();
+                        break;
+                }
+                switch (Suit)
+                {
+                    case Suit.Clubs:
+                        res += "♣";
+                        break;
+                    case Suit.Diamonds:
+                        res += "♦";
+                        break;
+                    case Suit.Hearts:
+                        res += "♥";
+                        break;
+                    case Suit.Spades:
+                        res += "♠";
+                        break;
+                    default:
+                        res += "X";
+                        break;
+                }
+                return res;
+            }
+        }
+
         /// <summary>
         /// Конструктор по-умолчанию. Создаст туза пик
         /// </summary>
@@ -34,8 +82,8 @@ namespace PlayingCards.Card
 
         public Card(int num)
         {
-            Suit = (Suit)(num/13) + 1;
-            Rank = (Rank)(num % 13) + 1;
+            Suit = (Suit)((num / 13) + 1);
+            Rank = (Rank)((num % 13) + 1);
         }
     }
 }
